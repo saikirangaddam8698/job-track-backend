9d05f9bdfe69970da9c0dd79c1217d502979b5de
SaikiranGaddam <saikiran.g8698@gmail.com>
2025-11-04 21:56:18 +0530

    added server configurations

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,11 +1,11 @@
-import { Request , Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-const authMiddleware = (req:Request, res:Response, next:NextFunction) => {
- const authHeader = req.headers.authorization;
- if (!authHeader || !authHeader.startsWith("Bearer ")) {
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "No token provided" });
   }
-    const token = authHeader.split(" ")[1];
+  const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET || "secretKey");
     (req as any).user = decoded;
@@ -13,4 +13,5 @@ const authMiddleware = (req:Request, res:Response, next:NextFunction) => {
   } catch (err) {
     return res.status(403).json({ message: "Invalid token" });
   }
-}
+};
+export default authMiddleware;
