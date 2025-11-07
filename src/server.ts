a78a65b2bc1f0a024dfc7f96cbc41f55d3ce8a0b
SaikiranGaddam <saikiran.g8698@gmail.com>
2025-11-07 12:15:05 +0530

    added files

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,8 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use(routes);
 
